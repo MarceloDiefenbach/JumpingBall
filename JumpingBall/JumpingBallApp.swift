99184e6a3663a58e6cecfbf6e693d0d9b22153d3
Marcelo Diefenbach <58398373+MarceloDiefenbach@users.noreply.github.com>
2022-11-08 13:00:15 -0300

    Initial Commit

---
+++ JumpingBall/JumpingBallApp.swift
@@ -0,0 +1,17 @@
+//
+//  JumpingBallApp.swift
+//  JumpingBall
+//
+//  Created by Marcelo Diefenbach on 08/11/22.
+//
+
+import SwiftUI
+
+@main
+struct JumpingBallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
